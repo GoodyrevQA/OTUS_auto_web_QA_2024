f57f95962d2ba5099f3e5f7fad2611bd868c2f58
Ivan <iegudyrev@gmail.com>
2024-06-22 15:51:10 +0300

    added hw_3

--- src/Rectangle.py
+++ src/Rectangle.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
-
-# test
+from math import pi
 
 
 class Figure(ABC):
@@ -20,6 +19,8 @@ class Figure(ABC):
 
 class Rectangle(Figure):
     def __init__(self, side_a, side_b):
+        if not isinstance(side_a, int | float) or not isinstance(side_b, int | float):
+            raise ValueError("Rectangle sides can only be numbers")
         if side_a <= 0 or side_b <= 0:
             raise ValueError("Rectangle sides can't be less than 0")
         self.side_a = side_a
@@ -36,12 +37,59 @@ class Rectangle(Figure):
 
 class Square(Rectangle):
     def __init__(self, side_a):
+        if not isinstance(side_a, int | float):
+            raise ValueError("Square sides can only be numbers")
         if side_a <= 0:
             raise ValueError("Square sides can't be less than 0")
         super().__init__(side_a, side_a)
+        # self.side_b = None
 
 
-r = Rectangle(3, 5)
-s = Square(5)
-print(r.add_area(s))
-print(s.add_area(r))
+class Circle(Figure):
+    def __init__(self, radius):
+        if not isinstance(radius, int | float):
+            raise ValueError("Circle radius can only be a number")
+        if radius <= 0:
+            raise ValueError("Circle radius can't be less than 0")
+        self.radius = radius
+
+    @property
+    def get_area(self):
+        return pi * self.radius**2
+
+    @property
+    def get_perimeter(self):
+        return 2 * pi * self.radius
+
+
+class Triangle(Figure):
+    def __init__(self, side_a, side_b, side_c):
+        if (
+            not isinstance(side_a, int | float)
+            or not isinstance(side_b, int | float)
+            or not isinstance(side_c, int | float)
+        ):
+            raise ValueError("Triangle sides can only be numbers")
+        if side_a <= 0 or side_b <= 0 or side_c <= 0:
+            raise ValueError("Triangle sides can't be less than 0")
+        if (
+            side_a + side_b <= side_c
+            or side_a + side_c <= side_b
+            or side_b + side_c <= side_a
+        ):
+            raise ValueError("Triangle sides can't be less than 0")
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+
+    @property
+    def get_area(self):
+        p = (self.side_a + self.side_b + self.side_c) / 2
+        return (p * (p - self.side_a) * (p - self.side_b) * (p - self.side_c)) ** 0.5
+
+    @property
+    def get_perimeter(self):
+        return self.side_a + self.side_b + self.side_c
+
+
+# алгоритм наследования классов - mro(). usage - print(Square.mro())
